c0ae5665c42e42c3ab10943ba6e2b7c4dab1da3a
yukisora <yukisora2013@gmail.com>
2016-01-28 23:26:18 +0800

    update

--- Spider/TiebaSpider.rb
+++ Spider/TiebaSpider.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 
 file = open("temp", "w")
+
 pageNum = 100
 pageNum.times do |i|
   page = Nokogiri::HTML(open("http://tieba.baidu.com/f?kw=c%E8%AF%AD%E8%A8%80&ie=utf-8&pn=#{i * 50}"))
@@ -14,7 +15,6 @@ pageNum.times do |i|
     if abstract == ''
       abstract = 'none'
     end
-    puts title.encoding
     file.write("#{title}\n")
     file.write("#{abstract}\n")
   end
