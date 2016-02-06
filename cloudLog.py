d52a37df56f78e70b2a459f3c248afb6bbc2b0d7
yukisora <yukisora2013@gmail.com>
2016-02-07 01:50:51 +0800

    cloud log

---
+++ cloudLog.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+import requests
+
+def postToCloud(threadData, key):
+    postdata = {
+        'title' : threadData['title'],
+        'author' : threadData['author'],
+        'abstract' : threadData['abstract'],
+        'tid' : threadData['tid'],
+        'pid' : threadData['pid'],
+        'replyNum' : threadData['replyNum'],
+        'operationTime' : threadData['operationTime'],
+        'operation' : threadData['operation'],
+        'grade' : threadData['grade'],
+        'keywords' : ', '.join(threadData['keywords']),
+        'operation' : threadData['operation'],
+        'operator' : key,
+
+    }
+
+    r = requests.post("http://yukisora.moe/tieba/post.php", data = postdata)
+    if r.content == 'poi':
+        return True
+    else:
+        return False
