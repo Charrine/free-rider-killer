43f105fdcd7577500ea0a7909e5d166ffedcc90c
yukisora <yukisora2013@gmail.com>
2016-03-25 07:21:21 +0800

    fix block hotplug

--- lib/autoBlock.py
+++ lib/autoBlock.py
@@ -13,15 +13,10 @@ from stdlog import stdLog
 def autoBlock(config):
 	stdLog(u'启动自动小黑屋', 'info')
 
-	stdLog(u'黑名单初始化中...', 'info')
-	blacklist = initBlacklist()
-	stdLog(u'黑名单初始化完毕', 'success')
-
 	stdLog(u'登录中...', 'info')
 	if adminLogin(config['user'], config['configFilename'][:-5] + '.co'):
 		stdLog(u'登陆成功', 'success')
 		while(True):
-			blacklist = initBlacklist()
 			s = sched.scheduler(time.time, time.sleep)
 			tomorrow = datetime.datetime.replace(datetime.datetime.now() +
 												datetime.timedelta(days = 1),
@@ -32,14 +27,18 @@ def autoBlock(config):
 			s.enter((tomorrow - datetime.datetime.now()).seconds,
 					1,
 					_block,
-					(config, blacklist))
+					(config,))
 			s.run()
-			_saveBlacklist(blacklist)
+
 	else:
 		stdLog(u'登陆失败', 'error')
 		sys.exit(1)
 
-def _block(config, blacklist):
+def _block(config):
+	stdLog(u'黑名单初始化中...', 'info')
+	blacklist = initBlacklist()
+	stdLog(u'黑名单初始化完毕', 'success')
+
 	for black in blacklist:
 		if black['times'] > 0:
 			print '%s %s' % (datetime.datetime.now().strftime('%y/%m/%d %H:%M:%S'), black['username'])
@@ -48,8 +47,10 @@ def _block(config, blacklist):
 					'你在%s吧黑名单中，剩余%d天解封。' % (config['forum']['kw'], black['times'] - 1))
 			black['times'] -= 1
 			time.sleep(5)
-		else:
+		if black['times'] == 0:
 			blacklist.remove(black)
+
+	_saveBlacklist(blacklist)
 
 def _saveBlacklist(blacklist):
 	with open('config/blacklist.txt', 'w') as f:
